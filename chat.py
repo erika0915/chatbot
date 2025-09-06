cbbfb24c7560f9f49012991fe2a1184b0ba4a868
erika0915 <soohee2001@gmail.com>
2025-09-06 17:21:52 +0900

    feat: add chat.py and llm.py(handle logics related to llm)

---
+++ chat.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from dotenv import load_dotenv
+from llm import get_ai_message 
+
+load_dotenv()
+
+st.set_page_config(page_title="소득세 챗봇", page_icon="🤖")
+
+st.title("🤖 소득세 챗봇")
+st.caption("소득세에 관련된 모든것을 답해드립니다.")
+
+if 'message_list' not in st.session_state:
+    st.session_state.message_list = []
+
+
+for message in st.session_state.message_list:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+    
+if user_question := st.chat_input(placeholder="소득세에 대해 궁금한 점을 물어보세요!"):
+    with st.chat_message("user"):
+        st.write(user_question)
+    st.session_state.message_list.append({"role": "user", "content": user_question})    
+
+    with st.spinner("AI가 답변을 작성하는 중..."):
+        ai_message = get_ai_message(user_question)
+        with st.chat_message("ai"):
+            st.write(ai_message)
+        st.session_state.message_list.append({"role": "ai", "content": "여기는 AI 메시지가 표시되는 곳입니다."})  
+
